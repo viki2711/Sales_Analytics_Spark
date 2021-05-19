0b63ff33bf2596e1f5eb3cc0517ede3c0d17ca3a
Vicky BRS <viki2711@gmail.com>
2021-05-19 12:23:15 -0400

    Add README, update main

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from parse_orders import parse_order, parse_orders
 from helpers import open_json
 import pandas as pd
 
+# creating a spark session
+spark = (SparkSession.builder.appName("OrdersDF").getOrCreate())
+
 # file path
 orders_file_path = "data/orders.txt"
 items_file_path = "data/item_data.json"
@@ -21,9 +24,6 @@ df_items = pd.DataFrame(items_data)
 data = clean_orders(orders_file_path)
 # parsing cleaned data to the correct data types
 parsed_data = parse_orders(parse_order, data)
-
-# creating a spark session
-spark = (SparkSession.builder.appName("OrdersDF").getOrCreate())
 
 # creating a data frame with cleaned parsed data and the schema
 orders_df = spark.createDataFrame(data=parsed_data, schema=schema)
@@ -52,15 +52,12 @@ item_df = orders_df.select("first_name", "last_name", "account_id", "item_id", "
 
 item = item_df.first()
 item_id = item["item_id"]
-item_base_cost = item["base_cost"]
+# fetching the correct info from pandas dataframe
 item_info = df_items[str(item_id)]
-quality = item_info["quality"]
-price = item_info["price"]
-name = item_info["itemName"]
 
 print(f'2. The most expensive item that user {user_id} purchased was: '
-      f'item_id: {item_id}, cost: {item_base_cost}$, '
-      f'item info - quality: {quality}, price: {price}$, name: {name}')
+      f'item_id: {item_id}, cost: {item["base_cost"]}$, '
+      f'item info - quality: {item_info["quality"]}, price: {item_info["price"]}$, name: {item_info["itemName"]}')
 
 # The most expensive item that user 143 purchased was: item_id = 2746, cost = 99.99$.
 
@@ -73,11 +70,16 @@ selected_users_df = (orders_df.select("first_name", "last_name", "account_id", "
           .agg(sum("price_paid").alias("sum_paid"))
 
 selected_users = selected_users_df.collect()
-for user in selected_users:
-    user_name = user["first_name"] + " " + user["last_name"]
-    account = user["account_id"]
-    sum_paid = round(user["sum_paid"], 2)
-    print(f'3. {user_name}, account_id: {account}, spent: {sum_paid}$')
 
+# formatting function
+def print_users(selected_users):
+    output = f'3. '
+    for user in selected_users:
+        user_name = user["first_name"] + " " + user["last_name"]
+        sum_paid = round(user["sum_paid"], 2)
+        output += f'{user_name}, account_id: {user["account_id"]}, spent: {sum_paid}$ | '
+    print(output)
+
+print_users(selected_users)
 # Marissa Washington account_id = 145 , spent 26.97$.
 # Marissa Washington account_id = 125, spent 18.45$.
